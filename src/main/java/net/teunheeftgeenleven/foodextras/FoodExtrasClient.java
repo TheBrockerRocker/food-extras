ce38f1ff9eed8d292f39054a1eac15a3c0b29e30
BuildTools <unconfigured@null.spigotmc.org>
2024-08-28 22:37:19 +0200

    Initial commit

---
+++ src/main/java/net/teunheeftgeenleven/foodextras/FoodExtrasClient.java
@@ -0,0 +1,10 @@
+package net.teunheeftgeenleven.foodextras;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FoodExtrasClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/teunheeftgeenleven/foodextras/FoodExtrasDataGenerator.java
@@ -0,0 +1,11 @@
+package net.teunheeftgeenleven.foodextras;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FoodExtrasDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
