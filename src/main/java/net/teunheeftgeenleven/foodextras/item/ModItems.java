784d9d77df4dc31c911c77a416ee38d2ea2cf9f6
BuildTools <unconfigured@null.spigotmc.org>
2024-09-02 10:30:19 +0200

    added cupcakes. added cupcake cream no recipes

--- src/main/java/net/teunheeftgeenleven/foodextras/item/ModItems.java
+++ src/main/java/net/teunheeftgeenleven/foodextras/item/ModItems.java
@@ -36,7 +36,22 @@ public class ModItems {
     public static final Item REMUF = registerItem("red_muffin", new Item(new Item.Settings()));
     public static final Item WMUF = registerItem("white_muffin", new Item(new Item.Settings()));
     public static final Item YMUF = registerItem("yellow_muffin", new Item(new Item.Settings()));
-
+    public static final Item BCRM = registerItem("black_cream", new Item(new Item.Settings()));
+    public static final Item BLCRM = registerItem("blue_cream", new Item(new Item.Settings()));
+    public static final Item BRCRM = registerItem("brown_cream", new Item(new Item.Settings()));
+    public static final Item CYCRM = registerItem("cyan_cream", new Item(new Item.Settings()));
+    public static final Item GCRM = registerItem("gray_cream", new Item(new Item.Settings()));
+    public static final Item GRCRM = registerItem("green_cream", new Item(new Item.Settings()));
+    public static final Item LBCRM = registerItem("light_blue_cream", new Item(new Item.Settings()));
+    public static final Item LGCRM = registerItem("light_gray_cream", new Item(new Item.Settings()));
+    public static final Item LCRM = registerItem("lime_cream", new Item(new Item.Settings()));
+    public static final Item MCRM = registerItem("magenta_cream", new Item(new Item.Settings()));
+    public static final Item OCRM = registerItem("orange_cream", new Item(new Item.Settings()));
+    public static final Item PCRM = registerItem("pink_cream", new Item(new Item.Settings()));
+    public static final Item PUCRM = registerItem("purple_cream", new Item(new Item.Settings()));
+    public static final Item RECRM = registerItem("red_cream", new Item(new Item.Settings()));
+    public static final Item WCRM = registerItem("white_cream", new Item(new Item.Settings()));
+    public static final Item YCRM = registerItem("yellow_cream", new Item(new Item.Settings()));
 
     private static Item registerItem(String name, Item item ) {
         return Registry.register(Registries.ITEM, new Identifier(FoodExtras.MOD_ID, name), item);
@@ -48,6 +63,22 @@ public class ModItems {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
             entries.add(KNIFE);
             entries.add(COCA);
+            entries.add(BCRM);
+            entries.add(BLCRM);
+            entries.add(BRCRM);
+            entries.add(CYCRM);
+            entries.add(GCRM);
+            entries.add(GRCRM);
+            entries.add(LBCRM);
+            entries.add(LGCRM);
+            entries.add(LCRM);
+            entries.add(MCRM);
+            entries.add(OCRM);
+            entries.add(PCRM);
+            entries.add(PUCRM);
+            entries.add(RECRM);
+            entries.add(WCRM);
+            entries.add(YCRM);
         });
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(entries -> {
             entries.add(PIZZA);
