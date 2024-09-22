ba2fdf2605b052cf55ec4915664a65475fc73645
BuildTools <unconfigured@null.spigotmc.org>
2024-09-22 15:50:00 +0200

    preparing for 2048 items added. :O

--- src/main/java/net/teunheeftgeenleven/foodextras/FoodExtrasClient.java
+++ src/main/java/net/teunheeftgeenleven/foodextras/FoodExtrasClient.java
@@ -2,11 +2,13 @@ package net.teunheeftgeenleven.foodextras;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.teunheeftgeenleven.foodextras.item.ModItems;
+import net.teunheeftgeenleven.foodextras.item.StirFry;
 
 public class FoodExtrasClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
 
         ModItems.registerModItems();
+        StirFry.registerModItems();
     }
 }
