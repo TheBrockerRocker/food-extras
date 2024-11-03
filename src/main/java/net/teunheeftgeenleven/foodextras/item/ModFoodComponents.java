3c226f6df7da0c9b61e611e9c3d8a04068b4c7d5
BuildTools <unconfigured@null.spigotmc.org>
2024-11-03 11:38:57 +0100

    ALL food for v1 is eatable

--- src/main/java/net/teunheeftgeenleven/foodextras/item/ModFoodComponents.java
+++ src/main/java/net/teunheeftgeenleven/foodextras/item/ModFoodComponents.java
@@ -34,6 +34,37 @@ public class ModFoodComponents {
     public static final FoodComponent STIR_FRY_T3 = new FoodComponent.Builder().nutrition(5).saturationModifier(3.5f).build();
     public static final FoodComponent STIR_FRY_T4 = new FoodComponent.Builder().nutrition(8).saturationModifier(5.3f).build();
     public static final FoodComponent STIR_FRY_T5 = new FoodComponent.Builder().nutrition(12).saturationModifier(8.0f).build();
+    public static final FoodComponent SUSHI = new FoodComponent.Builder().nutrition(5).saturationModifier(4.0f).build();
+    public static final FoodComponent DOGHNUT = new FoodComponent.Builder().nutrition(5).saturationModifier(4.0f).build();
+    public static final FoodComponent BREAD = new FoodComponent.Builder().nutrition(1).saturationModifier(0.5f).build();
+    public static final FoodComponent TOAST = new FoodComponent.Builder().nutrition(2).saturationModifier(1.0f).build();
+    public static final FoodComponent T2_TOAST = new FoodComponent.Builder().nutrition(3).saturationModifier(1.5f).build();
+    public static final FoodComponent T3_TOAST = new FoodComponent.Builder().nutrition(4).saturationModifier(2.0f).build();
+    public static final FoodComponent VEGGIE = new FoodComponent.Builder().nutrition(2).saturationModifier(0.5f).build();
+    public static final FoodComponent CUBE = new FoodComponent.Builder().nutrition(3).saturationModifier(2.5f).build();
+    public static final FoodComponent TACO = new FoodComponent.Builder().nutrition(5).saturationModifier(3.5f).build();
+    public static final FoodComponent FRUIT = new FoodComponent.Builder().nutrition(1).saturationModifier(3.0f).build();
+    public static final FoodComponent B_AND_E = new FoodComponent.Builder().nutrition(3).saturationModifier(3.0f).build();
+    public static final FoodComponent SMOOTIE = new FoodComponent.Builder().nutrition(2).saturationModifier(4.5f).build();
+    public static final FoodComponent BURGER = new FoodComponent.Builder().nutrition(4).saturationModifier(4.5f).build();
+    public static final FoodComponent CHOCO = new FoodComponent.Builder().nutrition(2).saturationModifier(2.5f).build();
+    public static final FoodComponent BACON = new FoodComponent.Builder().nutrition(2).saturationModifier(2.5f).build();
+    public static final FoodComponent EGG = new FoodComponent.Builder().nutrition(2).saturationModifier(2.5f).build();
+    public static final FoodComponent FRIES = new FoodComponent.Builder().nutrition(2).saturationModifier(2.5f).build();
+    public static final FoodComponent PANCAKE = new FoodComponent.Builder().nutrition(2).saturationModifier(2.5f).build();
+    public static final FoodComponent SAUSAGE = new FoodComponent.Builder().nutrition(2).saturationModifier(2.5f).build();
+    public static final FoodComponent HOTDOG = new FoodComponent.Builder().nutrition(2).saturationModifier(3.0f).build();
+    public static final FoodComponent CONE = new FoodComponent.Builder().nutrition(4).saturationModifier(2.5f).build();
+    public static final FoodComponent I_AND_S_CAKE = new FoodComponent.Builder().nutrition(6).saturationModifier(4.5f).build();
+    public static final FoodComponent JUICE = new FoodComponent.Builder().nutrition(1).saturationModifier(5.0f).build();
+    public static final FoodComponent SPAGHETTI = new FoodComponent.Builder().nutrition(3).saturationModifier(2.5f).build();
+    public static final FoodComponent SPAGHETTI_MEAT = new FoodComponent.Builder().nutrition(4).saturationModifier(3.5f).build();
+    public static final FoodComponent STRACCIATELLA = new FoodComponent.Builder().nutrition(3).saturationModifier(4.5f).build();
+    public static final FoodComponent STRAWBERRY = new FoodComponent.Builder().nutrition(3).saturationModifier(4.5f).build();
+    public static final FoodComponent SYRUP = new FoodComponent.Builder().nutrition(0).saturationModifier(6.5f).build();
+
+
+
 
     private static FoodComponent.Builder createStew(int hunger) {
         return new FoodComponent.Builder().nutrition(hunger).saturationModifier(0.6F);
